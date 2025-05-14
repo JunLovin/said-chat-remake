e96f01e72ecca06d8dfd0e5549a33289fcd7bc76
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-14 18:45:38 -0500

    feat(routing): add nested routes for Chat and Contacto pages
    Introduce nested routing for Chat and Contacto pages within the App component. Additionally, update the App component to conditionally render the Home page or nested routes based on the current location. Add modal styling for future use in the index.css file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { Outlet, useLocation } from "react-router";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Home from '@/pages/Home'
+
 function App() {
+  const location = useLocation()
+  const homepage = location.pathname === '/'
+
   return (
     <>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        {homepage ? (
+          <div className="home-container w-full flex-1 flex justify-center items-center">
+            <Home />
+          </div>
+        ) : (
+          <div className="outlet-container w-full flex-1 flex justify-center items-center">
+            <Outlet />
+          </div>
+        )}
+        <Footer />
+      </div>
     </>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,22 @@
 import App from "./App";
+import Chat from '@/pages/Chat'
+import Contacto from '@/pages/Contacto'
 
 const routes = [
     {
         path: '/',
-        element: <App/>
-    }
+        element: <App/>,
+        children: [
+            {
+                path: '/chat',
+                element: <Chat/>
+            },
+            {
+                path: '/contacto',
+                element: <Contacto/>
+            }
+        ]
+    },
 ]
 
 export default routes
