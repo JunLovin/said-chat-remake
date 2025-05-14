d3c07bf18752c654f48d8d961f31d6816a5af221
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-14 18:45:06 -0500

    feat: add WebSocket server for real-time communication
    This commit introduces a new WebSocket server to handle real-time communication between clients. The server listens on port 7173, manages connected users, and broadcasts messages to all active connections. It also logs user connections and disconnections for monitoring purposes.

---
+++ server.ts
@@ -0,0 +1,35 @@
+import { WebSocket, WebSocketServer } from 'ws'
+
+const ws = new WebSocketServer({ port: 7173 })
+
+const users = new Set<WebSocket>()
+
+ws.on('connection', (socket: WebSocket) => {
+    users.add(socket)
+    console.log("Usuario conectado")
+
+    socket.on('message', (msg:any) => {
+        let data
+        try {
+            data = JSON.parse(msg)
+        } catch (err) {
+            data = { nombre: "Desconocido", mensaje: msg, pfp: 'https://github.com/shadcn.png' }
+            console.error(err)
+        }
+        users.forEach(user => {
+            if (user.readyState === WebSocket.OPEN) {
+                user.send(JSON.stringify(data))
+            }
+        })
+        // socket.send(msg)
+    })
+
+    socket.on('close', () => {
+        users.delete(socket)
+        console.log("Usuario desconectado")
+    })
+})
+
+ws.on('listening', () => {
+    console.log("Servidor iniciado en ws:")
+})
